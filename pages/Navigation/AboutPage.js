7eee838ce447aadea82f530684f5553812e4baee
Christian Sheen <csheen1725@gmail.com>
2021-12-30 11:33:30 -0500

    random

--- pages/Navigation/AboutPage.js
+++ pages/Navigation/AboutPage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Box, Text } from "@chakra-ui/layout";
 
 const AboutPage = () => {
-  return <h1>About Page</h1>;
+  return (
+    <Box>
+      <h1>About Page</h1>
+      <h1>Our Mission</h1>
+      <Text>
+        Our mission is to serve the world’s most extraordinary
+        companies and extraordinary people who are building tomorrow’s
+        companies today. We have global reach, with a presence and
+        access to resources in Silicon Valley, China, Canada,
+        Columbia, Australia, India, Europe, and Southeast Asia. Our
+        broad global footprint enables our portfolio companies to
+        better understand future growth opportunities and expand
+        internationally. Of more than the 30 companies we have one
+        hundred percent owned or partnered with since our founding, a
+        third have either IPO’d or been acquired. And we have
+        celebrated at least one IPO a year since our inception.
+      </Text>
+    </Box>
+  );
 };
 
 export default AboutPage;
