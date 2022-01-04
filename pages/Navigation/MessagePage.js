001d8f15e5fde8eb8137b7434c137c51a0eb1be9
Christian Sheen <csheen1725@gmail.com>
2022-01-04 14:45:19 -0500

    Message.js

--- pages/Navigation/MessagePage.js
+++ pages/Navigation/MessagePage.js
@@ -1,7 +1,54 @@
+import { Box, Heading, Text } from "@chakra-ui/layout";
 import React from "react";
 
 const MessagePage = () => {
-  return <h1>Message Page</h1>;
+  return (
+    <Box>
+      <Heading>A Message from the CEO</Heading>
+      <Box w={600}>
+        <Text>
+          Veritas Venture Capital has been delivering sustainable
+          profit growth while building a solid business foundation
+          amidst a challenging venture capital environment. My mission
+          is to extend this trajectory into the future and turn
+          Veritas Venture Capital into one of the first decentralized
+          autonomous venture capital companies (DAVCC) running their
+          investments on the blockchain. We continue to face new
+          challenges in our investment environment as the pace and
+          scale of Regulation A+ investments are unprecedented. In
+          order to overcome these challenges, Veritas Venture Capital
+          has undergone an organizational transformation that includes
+          initiatives such as increased global investments and
+          restructuring sector portfolios to include blockchain
+          investments, cryptocurrencies, and biopharmaceuticals. These
+          improvements will enable us to become faster at identifying
+          and addressing shifts in the landscape. But there is more to
+          be done, and we remain fully committed to strengthening our
+          capabilities and to further evolving the organization going
+          forward. In addition, improved diversity among the team has
+          expanded our investment horizons. However, simply employing
+          diverse talent alone is not enough to energize an
+          organization. Nothing will change unless you have the right
+          culture conducive to everyone being able to bring their full
+          selves to work. Creating that environment is the
+          responsibility of me and the rest of the management team. We
+          have to adapt to provide the right products, services and
+          information according to the markets current situation. We
+          have to deliver all of this in a way that gets us the best
+          possible investment results in the proper sectors. Only when
+          these three pillars align with the company’s goals, do we
+          achieve the investment goals we are aiming for. Some people
+          take a negative view towards the future of financial
+          institutions and in particular venture capitalism. I am
+          confident the future holds boundless opportunities for the
+          Regulation A+ sector and its investment potential. As we
+          move forward towards this new future, Veritas Venture
+          Capital will work hard to realize sustainable growth for the
+          firm and society.
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default MessagePage;
